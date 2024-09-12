672a2d834b4c9d7fc1437d66ebd3db2c95f81607
4riz7 <vittmin47@gmail.com>
2024-09-12 13:39:08 +0300

    убрал не нужные функции

--- source/repos/Console_sort/Console_sort/Class1.cs
+++ source/repos/Console_sort/Console_sort/Class1.cs
@@ -11,32 +11,25 @@ namespace Console_sort
     {
         static void Main(string[] args)
         {
-            int dimensionality = 100000000;
+            int dimensionality = 100000;
             int[] array_b = create.crt_m_array(dimensionality);
 
             int[] array_r = (int[])array_b.Clone(); // Глубокая копия массива
 
-            Bubble(array_b);
 
-            void Bubble(int[] array)
-            {
-                Stopwatch stopwatch_Bubble = Stopwatch.StartNew();
-                Sorting.Bubble_Sort(array);
-                stopwatch_Bubble.Stop();
-                ulong swap_Bubble = Sorting.swaps_Bubble();
-                Console.WriteLine($"Время выполнения сортировки пузырьковым методом: {stopwatch_Bubble.ElapsedMilliseconds} мс\n Количество перестановок: {swap_Bubble}");
-            }
+            Stopwatch stopwatch_Bubble = Stopwatch.StartNew();
+            Sorting.Bubble_Sort(array_b);
+            stopwatch_Bubble.Stop();
+            ulong swap_Bubble = Sorting.swaps_Bubble();
+            Console.WriteLine($"Время выполнения сортировки пузырьковым методом: {stopwatch_Bubble.ElapsedMilliseconds} мс\n Количество перестановок: {swap_Bubble}");
+        
 
-            Rearr(array_r);
+            Stopwatch stopwatch_rearrangement = Stopwatch.StartNew();
+            rearrangement.InsertionSort(array_r);
+            stopwatch_rearrangement.Stop();
+            ulong swap_rearr = rearrangement.swap_rearr();
+            Console.WriteLine($"Время выполнения сортировки методом перестановки: {stopwatch_rearrangement.ElapsedMilliseconds} мс\n Количество перестановок: {swap_rearr}");
 
-            void Rearr(int[] array)
-            {
-                Stopwatch stopwatch_rearrangement = Stopwatch.StartNew();
-                rearrangement.InsertionSort(array);
-                stopwatch_rearrangement.Stop();
-                ulong swap_rearr = rearrangement.swap_rearr();
-                Console.WriteLine($"Время выполнения сортировки методом перестановки: {stopwatch_rearrangement.ElapsedMilliseconds} мс\n Количество перестановок: {swap_rearr}");
-            }
 
             Console.ReadLine();
             Console.ReadKey();
