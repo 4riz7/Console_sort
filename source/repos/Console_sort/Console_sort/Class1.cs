3132ce71d3cd0e41e341f4edd8e5305229edb8a9
4riz7 <vittmin47@gmail.com>
2024-09-14 03:21:52 +0300

    Чуть-чуть забыл)

--- source/repos/Console_sort/Console_sort/Class1.cs
+++ source/repos/Console_sort/Console_sort/Class1.cs
@@ -7,6 +7,7 @@ using Rearran;
 using Selections;
 using Merge_Sort;
 using Cocktail_Sort;
+using Quick_Sort;
 
 namespace Console_sort
 {
@@ -20,37 +21,45 @@ namespace Console_sort
             int[] array_s = (int[])array_b.Clone();
             int[] array_m = (int[])array_b.Clone();
             int[] array_c = (int[])array_b.Clone();
+            int[] array_q = (int[])array_b.Clone();
+
 
             Stopwatch stopwatch_Bubble = Stopwatch.StartNew();
             Sorting.Bubble_Sort(array_b);
             stopwatch_Bubble.Stop();
             ulong swap_Bubble = Sorting.swaps_Bubble();
-            Console.WriteLine($"Время выполнения сортировки пузырьковым методом: {stopwatch_Bubble.ElapsedMilliseconds} мс\n Количество перестановок: {swap_Bubble}");
+            Console.WriteLine($"Время выполнения методом пузырьковой сортировки: {stopwatch_Bubble.ElapsedMilliseconds} мс\n Количество перестановок: {swap_Bubble}");
         
 
             Stopwatch stopwatch_rearrangement = Stopwatch.StartNew();
             rearrangement.InsertionSort(array_r);
             stopwatch_rearrangement.Stop();
             ulong swap_rearr = rearrangement.swap_rearr();
-            Console.WriteLine($"Время выполнения сортировки методом перестановки: {stopwatch_rearrangement.ElapsedMilliseconds} мс\n Количество перестановок: {swap_rearr}");
+            Console.WriteLine($"Время выполнения методом сортировки перестановки: {stopwatch_rearrangement.ElapsedMilliseconds} мс\n Количество перестановок: {swap_rearr}");
 
             Stopwatch stopwatch_selection = Stopwatch.StartNew();
             selection.SelectionSort(array_s);
             stopwatch_selection.Stop();
             ulong swap_select = selection.swap_select();
-            Console.WriteLine($"Время выполнения сортировки методом выбора: {stopwatch_selection.ElapsedMilliseconds} мс\n Количество перестановок: {swap_select}");
+            Console.WriteLine($"Время выполнения методом сортировки выбора: {stopwatch_selection.ElapsedMilliseconds} мс\n Количество перестановок: {swap_select}");
 
             Stopwatch stopwatch_merge = Stopwatch.StartNew();
             merge.MergeSort(array_m);
             stopwatch_merge.Stop();
             ulong swap_merge = merge.swap_merge();
-            Console.WriteLine($"Время выполнения сортировки методом слияния: {stopwatch_merge.ElapsedMilliseconds} мс\n Количество перестановок: {swap_merge}");
+            Console.WriteLine($"Время выполнения методом сортировки слияния: {stopwatch_merge.ElapsedMilliseconds} мс\n Количество перестановок: {swap_merge}");
 
             Stopwatch stopwatch_cocktail = Stopwatch.StartNew();
             cocktail.CocktailSort(array_c);
             stopwatch_cocktail.Stop();
             ulong swap_cocktail = cocktail.swap_cocktail();
-            Console.WriteLine($"Время выполнения сортировки методом шейкерирования: {stopwatch_merge.ElapsedMilliseconds} мс\n Количество перестановок: {swap_cocktail}");
+            Console.WriteLine($"Время выполнения методом шейкерной сортировки: {stopwatch_merge.ElapsedMilliseconds} мс\n Количество перестановок: {swap_cocktail}");
+
+            Stopwatch stopwatch_quick = Stopwatch.StartNew();
+            quick.QuickSort(array_q);
+            stopwatch_cocktail.Stop();
+            ulong swap_quick = quick.swap_quick();
+            Console.WriteLine($"Время выполнения методом быстрой сортировки: {stopwatch_quick.ElapsedMilliseconds} мс\n Количество перестановок: {swap_quick}");
 
             Console.ReadLine();
             Console.ReadKey();
---
+++ source/repos/Console_sort/Console_sort/QuickSort.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+
+namespace Quick_Sort
+{
+    internal class quick
+    {
+        static ulong swapCount = 0;
+
+        // Основной метод быстрой сортировки
+        public static void QuickSort(int[] array)
+        {
+            swapCount = 0;
+            QuickSortHelper(array, 0, array.Length - 1);
+            PrintArray(array);
+        }
+
+        // Рекурсивный вспомогательный метод
+        private static void QuickSortHelper(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = Partition(array, low, high);
+                QuickSortHelper(array, low, pivotIndex - 1);
+                QuickSortHelper(array, pivotIndex + 1, high);
+            }
+        }
+
+        // Метод разделения массива и выбор опорного элемента
+        private static int Partition(int[] array, int low, int high)
+        {
+            int pivot = array[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (array[j] < pivot)
+                {
+                    i++;
+                    Swap(ref array[i], ref array[j]);
+                }
+            }
+
+            Swap(ref array[i + 1], ref array[high]);
+            return i + 1;
+        }
+
+        // Вспомогательный метод для обмена элементов массива
+        private static void Swap(ref int firstArg, ref int secondArg)
+        {
+            int temp = firstArg;
+            firstArg = secondArg;
+            secondArg = temp;
+            swapCount++;
+        }
+
+        // Метод для записи отсортированного массива в файл
+        public static void PrintArray(int[] array)
+        {
+            StreamWriter quick_array = new StreamWriter("C:\\Users\\vittm\\source\\repos\\Console_sort\\quick_array.txt");
+            for (int i = 0; i < array.Length; i++)
+            {
+                quick_array.Write(array[i] + " ");
+            }
+            quick_array.Close();
+        }
+
+        // Метод для получения количества перестановок
+        public static ulong swap_quick()
+        {
+            return swapCount;
+        }
+    }
+}
